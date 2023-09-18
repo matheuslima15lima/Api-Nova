a797d0758679ffe82755f45e244b25b53d19d118
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-09-18 17:26:18 -0300

    adicionando nova api Event +

---
+++ Event+_Tarde_CodeFirst/webapi.event+.tarde/Domains/ComentarioEvento.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.tarde.Domains
+{
+    [Table(nameof(ComentarioEvento))]
+    public class ComentarioEvento
+    {
+        [Key]
+        public Guid IdComentarioEvento { get; set; }
+
+        [Column(TypeName = "TEXT")]
+        [Required(ErrorMessage = "Descrição obrigatória!")]
+        public string? Descricao { get; set; }
+
+        [Column(TypeName = "BIT")]
+        [Required(ErrorMessage = "Informação sobre exibição obrigatória!")]
+        public bool Exibe { get; set; }
+
+        //ref.tabela Usuario
+        [Required(ErrorMessage = " Usuario obrigatório!")]
+        public Guid IdUsuario { get; set; }
+
+        [ForeignKey(nameof(IdUsuario))]
+        public Usuario? Usuario { get; set; }
+
+        //ref.tabela Evento
+
+        [Required(ErrorMessage = " Evento obrigatório!")]
+        public Guid IdEvento { get; set; }
+
+        [ForeignKey(nameof(IdEvento))]
+        public Evento? Evento { get; set; }
+
+    }
+}
---
+++ Event+_Tarde_CodeFirst/webapi.event+.tarde/Domains/TipoUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.tarde.Domains
+{
+    [Table(nameof(TipoUsuario))]
+    public class TipoUsuario
+    {
+        [Key]
+        public Guid IdTipoUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Titulo do tipo de Usuario Obrigatorio!!!")]
+        public string? Titulo { get; set; }
+
+    }
+}
